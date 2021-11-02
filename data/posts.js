87104553df870d287c735a6a321d7f507ebac4c3
itsrealoj <stephenojwang1040@gmail.com>
2021-11-02 10:48:07 +0300

    db and config

---
+++ data/posts.js
@@ -0,0 +1,11 @@
+const posts = [
+    {
+        name: 'I love fleetsimplified',
+        likesCount: 0,
+        unlikesCount: 0,
+        likes: ['Jerry Doe', 'Mary Doe'],
+        unlikes: ['Laban dev', 'Cyril Okidi'],
+        postedBy: 'John Doe',
+        postedAt: new Date()
+      },
+];
